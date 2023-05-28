8d816cd945fa56a6e4f3a521e88ac7bd40292cfe
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-05-28 19:24:09 +0200

    box added (raw)

--- src/Models/ModelOne.js
+++ src/Models/ModelOne.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import Model from '../components/ok'
 const ModelOne = () => {
     return (
         <section className='row'>
             <h1 className='col-4'>Model one</h1>
-            <div className='col-8'>w</div>
+            <div className='col-8'>
+                <Model />
+            </div>
         </section>
     )
 }
