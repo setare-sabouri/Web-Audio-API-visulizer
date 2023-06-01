ded227a55ccac31d849801f46d86042119b9e54f
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-06-01 17:15:41 +0200

    analyser passed as prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import React, { useEffect, useState, useRef } from 'react';
 import './App.css';
 import WaveComponent from './components/Wave/wave';
 import CubeComponent from './components/cubeOne';
 function App() {
+  const [analyser, setAnalyser] = useState(null);
+  const mediaStreamRef = useRef(null);
+
+  useEffect(() => {
+    let audioContext = null;
+    let animationFrameId = null;
+
+    const initializeAnalyser = async () => {
+      try {
+        const mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        mediaStreamRef.current = mediaStream;
+        audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const analyser = audioContext.createAnalyser();
+        const source = audioContext.createMediaStreamSource(mediaStream);
+        source.connect(analyser);
+        setAnalyser(analyser);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    initializeAnalyser();
+
+    return () => {
+      if (audioContext) {
+        audioContext.close();
+      }
+      if (mediaStreamRef.current) {
+        mediaStreamRef.current.getTracks().forEach((track) => track.stop());
+      }
+      if (animationFrameId) {
+        cancelAnimationFrame(animationFrameId);
+      }
+    };
+  }, []);
 
   return (
     <>
-      <WaveComponent />
+      {analyser && <WaveComponent analyser={analyser} />}
       <CubeComponent />
     </>
   );
--- src/components/Wave/wave.js
+++ src/components/Wave/wave.js
@@ -1,98 +1,55 @@
 import React, { useEffect, useRef } from 'react';
-import './wave.css'
-import CubeOne from '../cubeOne';
-import { Canvas } from '@react-three/fiber';
+import './wave.css';
 
-const WaveComponent = () => {
+const WaveComponent = ({ analyser }) => {
     const canvasRef = useRef(null);
 
     useEffect(() => {
-        let mediaStream = null;
-        let audioContext = null;
-        let analyser = null;
-        let animationFrameId = null;
+        const canvas = canvasRef.current;
+        const { width, height } = canvas;
+        const canvasContext = canvas.getContext('2d');
 
-        const startVisualization = async () => {
-            try {
-                //-------Analyser Setup----
+        const pixelRatio = window.devicePixelRatio || 1;
 
-                mediaStream = await navigator.mediaDevices.getUserMedia({ audio: true });
-                audioContext = new (window.AudioContext || window.webkitAudioContext)();
-                analyser = audioContext.createAnalyser();
-                const source = audioContext.createMediaStreamSource(mediaStream);
-                source.connect(analyser);
+        canvas.width = width * pixelRatio;
+        canvas.height = height * pixelRatio;
+        canvasContext.scale(pixelRatio, pixelRatio);
 
-                //-------------------------
+        const draw = () => {
+            const bufferLength = analyser.frequencyBinCount;
+            const dataArray = new Uint8Array(bufferLength);
 
-                const canvas = canvasRef.current;
-                const { width, height } = canvas;
-                const canvasContext = canvas.getContext('2d');
+            analyser.getByteFrequencyData(dataArray);
 
-                const pixelRatio = window.devicePixelRatio || 1;
+            canvasContext.clearRect(0, 0, width, height);
+            canvasContext.fillStyle = '#602060';
+            canvasContext.fillRect(0, 0, width, height);
 
-                canvas.width = width * pixelRatio;
-                canvas.height = height * pixelRatio;
-                canvasContext.scale(pixelRatio, pixelRatio);
+            const barWidth = (width / bufferLength) * 10;
+            let x = 0;
 
-                const draw = () => {
+            //------------- to draw bars -------------------
 
-                    const bufferLength = analyser.frequencyBinCount;
-                    const dataArray = new Uint8Array(bufferLength);
+            for (let i = 0; i < bufferLength; i++) {
+                const barHeight = (dataArray[i] / 255) * height;
+                canvasContext.strokeStyle = '#ecc6ec';
+                canvasContext.lineWidth = 2;
+                canvasContext.strokeRect(x, height - barHeight / 2, barWidth, barHeight / 2);
 
-                    analyser.getByteFrequencyData(dataArray);
-
-                    canvasContext.clearRect(0, 0, width, height);
-                    canvasContext.fillStyle = '#602060';
-                    canvasContext.fillRect(0, 0, width, height);
-
-                    const barWidth = (width / bufferLength) * 10;
-                    let x = 0;
-
-                    //------------- to draw bars ------------------- 
-
-                    for (let i = 0; i < bufferLength; i++) {
-                        const barHeight = (dataArray[i] / 255) * height;
-                        canvasContext.strokeStyle = '#ecc6ec';
-                        canvasContext.lineWidth = 2;
-                        canvasContext.strokeRect(x, height - barHeight / 2, barWidth, barHeight / 2);
-
-
-                        x += barWidth + 1;
-                    }
-                    animationFrameId = requestAnimationFrame(draw);
-
-                    //-----------------------------------------------
-                };
-
-                draw();
-            } catch (error) {
-                console.log(error.message);
+                x += barWidth + 1;
             }
+
+            requestAnimationFrame(draw);
         };
 
-        startVisualization();
-
-        return () => {
-            if (mediaStream) {
-                mediaStream.getTracks().forEach((track) => track.stop());
-            }
-            if (audioContext) {
-                audioContext.close();
-            }
-            if (animationFrameId) {
-                cancelAnimationFrame(animationFrameId);
-            }
-        };
-    }, []);
+        draw();
+    }, [analyser]);
 
     return (
         <div className='waveContainer'>
-            {/* <Canvas>
-                <CubeOne />
-            </Canvas> */}
-            <canvas ref={canvasRef} className='canvas'></canvas>;
+            <canvas ref={canvasRef} className='canvas'></canvas>
         </div>
-    )
+    );
 };
 
 export default WaveComponent;
